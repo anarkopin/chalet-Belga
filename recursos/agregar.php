e54ca4276412a3e3c4594b4e37bfd1b0b1889273
Diego Bejar <diebejardelaguila@gmail.com>
2021-07-22 22:21:57 -0500

    Conexion Reclamos a la base de datos

--- recursos/agregar.php
+++ recursos/agregar.php
@@ -30,9 +30,58 @@ if(isset($_POST['aceptar'])){
             <h3 class="bad">!Por favor completa los campos¡</h3>
             <?php
         }
-    } else if (isset($_POST['enviar'])) {
+} else if (isset($_POST['enviar'])) {
+    if (
+    strlen($_POST['nombre']) >= 1 && 
+    strlen($_POST['apellido']) >=1 && 
+    strlen($_POST['direccion']) && 
+    strlen($_POST['distrito']) >= 1 && 
+    strlen($_POST['documento_identidad']) >=1 && 
+    strlen($_POST['numero_documento']) >=1 && 
+    strlen($_POST['correo']) >=1 && 
+    strlen($_POST['telefeno_alternativo']) >=1 && 
+    strlen($_POST['nombre_papa']) >=1 && 
+    strlen($_POST['monto_reclamo']) >=1 && 
+    strlen($_POST['tipo']) >=1 && 
+    strlen($_POST['descripcion']) >=1
+    ) {
 
 
-    }       
+        $nombre =$_POST["nombre"];
+        $apellido =$_POST["apellido"];
+        $direccion =$_POST["direccion"];
+        $distrito =$_POST["distrito"];
+        $documento_iden =$_POST["documento_identidad"];
+        $numero_documento =$_POST["numero_documento"];
+        $correo =$_POST["correo"];
+        $telefono_dos =$_POST["telefeno_alternativo"];
+        $nombre_papa =$_POST["nombre_papa"];
+        $monto_reclamo =$_POST["monto_reclamo"];
+        $pro_ser =$_POST["tipo"];
+        $descripcion =$_POST["descripcion"];
+        $id = rand(1,99);
+
+        $insertarDatos = "INSERT INTO reclamaciones(nombre, apellido, direccion, distrito, documento_identidad, numero_documento, correo, telefeno_alternativo, nombre_papa, monto_reclamo, producto_servicio, descripcion,id) VALUES ('$nombre','$apellido','$direccion','$distrito','$documento_iden','$numero_documento','$correo','$telefono_dos','$nombre_papa','$monto_reclamo','$pro_ser','$descripcion', '$id')";
+
+        $ejecutaInsertar = mysqli_query($enlace, $insertarDatos);
+
+        if ($ejecutaInsertar) {
+            ?>
+            <h3 class="ok">Gracias por ponerte en contacto con nosotros, tu opinion es valiosa</h3>
+            <?php
+        
+        } else {
+            ?>
+            <h3 class="bad">¡Ups ha ocurrido un error!</h3>
+            <?php
+        }
+
+    } else {
+        ?>
+        <h3 class="bad">!Por favor completa los campos¡</h3>
+        <?php
+    }
+
+}       
 
 ?>
--- vistas/reclamos.php
+++ vistas/reclamos.php
@@ -1,3 +1,6 @@
+<?php
+   require_once('../recursos/conectar.php');
+?>
 <html>
 <head>
 	<meta charset="UTF-8">
@@ -7,26 +10,26 @@
 <body>
 	<div class="container">
 		<h1 class="text-center">Libro de reclamaciones</h1>
-		<form class="col-sm-6 col-sm-offset-3" action="reclamacion.php" method="POST">
+		<form class="col-sm-6 col-sm-offset-3" method="POST">
 			<div class="form-group">
 				<label>Nombre *</label>
-				<input type="text" class="form-control" placeholder="Ej. Cesar" name="">
+				<input type="text" class="form-control" placeholder="Ej. Cesar" name="nombre">
 			</div>
 			<div class="form-group">
 				<label>Apellido *</label>
-				<input type="text" class="form-control" placeholder="Ej. Aquino Maximiliano" name="">
+				<input type="text" class="form-control" placeholder="Ej. Aquino Maximiliano" name="apellido">
 			</div>
 			<div class="form-group">
 				<label>Dirección *</label>
-				<input type="text" class="form-control" placeholder="Ej. Av. Los Angeles 1025" name="">
+				<input type="text" class="form-control" placeholder="Ej. Av. Los Angeles 1025" name="direccion">
 			</div>
 			<div class="form-group">
 				<label>Distrito *</label>
-				<input type="text" class="form-control" placeholder="Ej. Villa El Salvador" name="">
+				<input type="text" class="form-control" placeholder="Ej. Villa El Salvador" name="distrito">
 			</div>
 			<div class="form-group">
 				<label>Documento de Identidad*</label>
-				<select class="form-control" name="">
+				<select class="form-control" name="documento_identidad">
 					<option>-Ninguno-</option>
 					<option value="D.N.I.">D.N.I.</option>
 					<option value="C.E.">C.E.</option>
@@ -35,37 +38,37 @@
 			</div>
 			<div class="form-group">
 				<label>N° doc. Identidad *</label>
-				<input type="text" class="form-control" placeholder="Ej. 40125201" name="">
+				<input type="text" class="form-control" placeholder="Ej. 40125201" name="numero_documento">
 			</div>
 			<div class="form-group">
 				<label>Correo electrónico *</label>
-				<input type="email" class="form-control" placeholder="Ej. nombre@correo.com" name="">
+				<input type="email" class="form-control" placeholder="Ej. nombre@correo.com" name="correo">
 			</div>
 			<div class="form-group">
 				<label>Teléfono alternativo *</label>
-				<input type="text" class="form-control" placeholder="Ej. 1 294-0008" name="">
+				<input type="text" class="form-control" placeholder="Ej. 1 294-0008" name="telefeno_alternativo">
 			</div>
 			<div class="form-horizontal">
 				<div class="form-group">
 					<label class="col-md-3">Papá Mamá *</label>
 					<div class="col-md-9">
-						<input type="text" class="form-control" placeholder="Ej. Luis" name="">
+						<input type="text" class="form-control" placeholder="Ej. Luis" name="nombre_papa">
 					</div>
 				</div>
 			</div>
 			<div class="form-group">
 				<label>Monto a reclamar (S/.)</label>
-				<input type="text" class="form-control" placeholder="Ej. 200" name="">
+				<input type="text" class="form-control" placeholder="Ej. 200" name="monto_reclamo">
 			</div>
 			<label class="radio-inline">
-				<input type="radio" name="tipo" checked> Producto
+				<input type="radio" name="tipo" value="producto" checked> Producto
 			</label>
 			<label class="radio-inline">
-				<input type="radio" name="tipo"> Servicio
+				<input type="radio" name="tipo" value="servicio"> Servicio
 			</label>
 			<div class="form-group">
 				<label>Descripcion</label>
-				<textarea class="form-control" rows="5" name=""></textarea>
+				<textarea class="form-control" rows="5" name="descripcion"></textarea>
 			</div>
 			<input type="submit" class="btn btn-block btn-lg btn-primary" name="enviar">
 		</form>
@@ -74,3 +77,6 @@
 	</div>
 </body>
 </html>
+<?php
+    require_once('../recursos/agregar.php');
+?>
